a8f07bc139fd61325c0b5044b3aef43998526c7a
GeorgiySurkov <georgiy.surkov@gmail.com>
2021-03-02 09:06:33 +0000

    add use_hidden_pooling in model and shuffle dataloader

--- model_level/models/sent_pair_binary_classifier.py
+++ model_level/models/sent_pair_binary_classifier.py
@@ -7,8 +7,15 @@ from typing import Tuple, List, Union
 
 class SentPairBinaryClassifier(nn.Module):
     # TODO: (architecture) make transformer model to be a composition of transformer and head
-    def __init__(self, mname: str, cache_dir="./cache_models/",
-                 droprate=0.3, head_nlayers=1, head_nneurons=768):
+    def __init__(
+            self,
+            mname: str,
+            cache_dir: str = "./cache_models/",
+            droprate: int = 0.3,
+            head_nlayers: int = 1,
+            head_nneurons: int = 768,
+            use_hidden_pooling: bool = False
+    ):
         """
         :param mname: a Model name listed in https://huggingface.co/models
         """
@@ -23,6 +30,7 @@ class SentPairBinaryClassifier(nn.Module):
         self.transformer_out_size = self._get_transformer_out_size(self.transformer)
 
         self.head = self._create_head(droprate, head_nlayers, head_nneurons)
+        self.use_hidden_pooling = use_hidden_pooling
 
     def get_init_kwargs(self):
         return {"mname": self.mname,
@@ -41,48 +49,22 @@ class SentPairBinaryClassifier(nn.Module):
         """
         """
         x = self.transformer(*transformer_inputs)
-        x = x["pooler_output"]  # be attentive: last_hidden_state isn't used for classification
+        if self.use_hidden_pooling:
+            x = x['last_hidden_state']
+        else:
+            x = x["pooler_output"]  # be attentive: last_hidden_state isn't used for classification
         x = self.head(x)
         return x
 
     def _create_head(self, droprate: float, n_layers: int, head_nneurons: int) -> nn.Module:
         if n_layers > 1:
             raise ValueError(n_layers)
-        head = nn.Sequential(
-            nn.Dropout(p=droprate),
-            nn.Linear(self.transformer_out_size, 2)
-        )
-        return head
-
-    # def _create_head(self, droprate: float, n_layers: int, head_nneurons: int) -> nn.Module:
-    #     head_layers = []
-    #
-    #     for layer_idx in range(n_layers):
-    #         is_first = layer_idx == 0
-    #         is_last = layer_idx == (n_layers - 1)
-    #         layers = self._create_head_layer_components(is_first, is_last, droprate, head_nneurons)
-    #         head_layers += layers
-    #
-    #     return nn.Sequential(*head_layers)
-    #
-    # def _create_head_layer_components(self, is_first: bool, is_last: bool, droprate: float, nneurons: int
-    #                                   ) -> List[nn.Module]:
-    #     inp_hidden_size, out_hidden_size = nneurons, nneurons
-    #     if is_first:
-    #         inp_hidden_size = self.transformer_out_size
-    #     if is_last:
-    #         out_hidden_size = 1
-    #
-    #     linear = nn.Linear(inp_hidden_size, out_hidden_size)
-    #     dropout = nn.Dropout(droprate)
-    #     comps = [dropout, linear]
-    #     if is_last:
-    #         comps.append(nn.Sigmoid())
-    #     else:
-    #         activation = nn.ReLU()
-    #         comps.append(activation)
-    #
-    #     return comps
+        layers = []
+        if self.use_hidden_pooling:
+            layers.append(nn.AdaptiveAvgPool2d(output_size=(1, self.transformer_out_size)))
+        layers.append(nn.Dropout(p=droprate))
+        layers.append(nn.Linear(self.transformer_out_size, 2))
+        return nn.Sequential(*layers)
 
     def reset_weights(self, device: Union[None, torch.device] = None) -> None:
         self.transformer = self._load_transformer()
