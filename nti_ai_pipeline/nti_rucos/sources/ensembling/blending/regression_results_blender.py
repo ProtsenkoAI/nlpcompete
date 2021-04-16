98dd0ea7afdb0bbdd586db59d2dc65b272b9f794
Protsenko Arseny <procenko_03@mail.ru>
2021-04-16 22:01:24 +0500

    realization of regression results blender

--- nti_ai_pipeline/nti_rucos/sources/ensembling/blending/regression_results_blender.py
+++ nti_ai_pipeline/nti_rucos/sources/ensembling/blending/regression_results_blender.py
@@ -3,12 +3,16 @@ import numpy as np
 
 
 class RegressionResultsBlender:
-    # TODO
     r"""
     Takes already made predictions of every model and returns blended predictions.
     """
-    def __init__(self):
-        ...
-
     def blend(self, models_preds: List[Sequence], models_weights: List[Union[float, int]]) -> np.array:
-        raise NotImplementedError
+        self._validate_preds_and_weights(models_preds, models_weights)
+        preds_arr = np.array(models_preds)
+        normalized_weights = np.array(models_weights) / sum(models_weights)
+        preds_weighted = preds_arr * normalized_weights
+        return preds_weighted
+
+    def _validate_preds_and_weights(self, preds, weights):
+        if len(preds) != len(weights):
+            raise ValueError("Number of model weights not equal to number of predictions objects.")
